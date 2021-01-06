affdfa9143f8de54091d46cc8bbccdab4ae2f520
Vuyisanani <vuyisanani@pop-os.localdomain>
2021-01-06 23:28:06 +0200

    added components with rready functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import QuestionCard  from './components/QuestionCard';
 
-function App() {
-  
+const App = () => {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number,setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1> My Quiz App</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score"> Score: </p>
+      <p>Loading Questios..</p>
+      <QuestionCard />
+
+      <button className="next" onClick={nextQuestion}>Next</button>
+
     </div>
   );
 }
---
+++ src/components/Api.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Api = () => { 
+    
+    return (
+        <div> 
+            Api
+        </div>
+    )
+}
+
+export default Api;
