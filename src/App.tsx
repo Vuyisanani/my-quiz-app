5cbacb8f40ede91167af978a6cba45c8917b559d
Vuyisanani <vuyisanani@pop-os.localdomain>
2021-01-07 11:30:00 +0200

    added a call to the api that return a Promise

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import QuestionCard  from './components/QuestionCard';
+import { fetchQuizQuestions, Difficulty }from './components/Api';
 
 const App = () => {
 
@@ -11,6 +12,8 @@ const App = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
 
   const startTrivia = async () => {
 
@@ -32,14 +35,14 @@ const App = () => {
       </button>
       <p className="score"> Score: </p>
       <p>Loading Questios..</p> 
-      <QuestionCard 
+      {/* <QuestionCard 
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAswer}
-      />
+      /> */}
 
       <button className="next" onClick={nextQuestion}>Next</button>
 
